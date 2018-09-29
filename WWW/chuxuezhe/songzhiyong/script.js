31ee368715476d68dda23afa1a6fd4bf75b851f6
Duke <duke123@aliyun.com>
2018-09-29 08:26:10 +0800

    update

---
+++ WWW/chuxuezhe/songzhiyong/script.js
@@ -0,0 +1,15 @@
+function fibonacci(n){
+    if(n<2)return n;
+    return fibonacci(n-1)+fibonacci(n-2)
+}
+
+function displayDate() {
+    document.getElementById("date").innerHTML = Date();
+}
+
+function start(){
+    // alert(fibonacci(10))
+    setInterval(displayDate, 60);
+}
+
+onload = start;
