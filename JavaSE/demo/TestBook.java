f65077e3739a3b9ca1a863eb2bab7f113b042943
Duke <duke123@aliyun.com>
2018-09-29 09:40:05 +0800

    update

---
+++ JavaSE/demo/TestBook.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Book{
+    String author,name;
+    double price;
+
+    void add(String author,String name,double price){
+        this.author=author;
+        this.name=name;
+        this.price=price;
+    }
+
+    void display(){
+        System.out.println(author+"\t"+name+"\t"+price);
+    }
+
+}
+
+public class TestBook{
+    public static void main(String[] args){
+        int number=2;
+        Scanner sc = new Scanner(System.in);
+        Book[] book = new Book[number];
+        book[0].add(sc.next(),sc.next(),sc.nextInt());
+        book[0].display();
+    }
+}
