fe06be79ff6a4bb286d566a3284a7c00c1022867
DukeBode <Duke123@aliyun.com>
2019-10-08 14:41:05 +0800

    delete

--- JavaSE/demo/Day.java
+++
@@ -1,8 +0,0 @@
-public class Day{
-    public static void main(String[] args){
-        int day[]={31,28,31,30,31,30,31,31,30,31,30,31};
-        for(int i:day){
-            System.out.println((i+1)+day[i]+"天");
-        }
-    }
-}
